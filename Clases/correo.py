210f783a3432d27c312013d23586a9f83bd63b40
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-03 16:06:17 +0200

    nose q meter en except pqno encuentroerror comotal

--- Clases/correo.py
+++ Clases/correo.py
@@ -1,8 +1,11 @@
 import re #recibe una cadena y regresa un booleano indicando si es un correo electrónico válido o no
 class Correoelectronico:
-    def __init__(self,correo,):
+    def __init__(self,correo):
         self.correo = correo
+        self.correos = ["flecha@gmail.com","rodri@gmail.com","jose@gmail.com"]
+
         
     def search1(self):
-        if re.search("@",self.correo):
+        if re.search("@*.*com*",self.correo):
             print("Bienvenido a la web")
+        
