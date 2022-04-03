9912d0e6802defb741d6b06a6b8d89f069728682
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-03 12:40:14 +0200

    probando

--- Clases/correo.py
+++ Clases/correo.py
@@ -1,9 +1,18 @@
-import re
+import re #recibe una cadena y regresa un booleano indicando si es un correo electrónico válido o no
 class Correoelectronico:
     def __init__(self,correo):
         self.correo = correo
-    def search(self):
-        return re.search(". * @. * \ .. *", tu_correo)
-    
-    
-tu_correo = input("Introduzca su correo para poder iniciar sesion: ")
+    def search1():
+        return re.search("@",correo)
+    def validacion():
+        search1()
+        if re.search == True:
+            print("hola")
+        elif re.search == False:
+            print("Adios")
+
+
+
+correo = input("Introduzca su correo para poder iniciar sesion: ")
+ff= Correoelectronico(correo)
+print(ff.validacion())
