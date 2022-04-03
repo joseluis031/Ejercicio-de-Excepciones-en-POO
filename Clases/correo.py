0daaaebbdbd14cfbbbae9ff8935c1a4c40676e5d
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-03 11:20:02 +0200

    utilizo metodo search y meto el input

--- Clases/correo.py
+++ Clases/correo.py
@@ -2,4 +2,8 @@ import re
 class Correoelectronico:
     def __init__(self,correo):
         self.correo = correo
-        
+    def search(self):
+        return re.search(". * @. * \ .. *", tu_correo)
+    
+    
+tu_correo = input("Introduzca su correo para poder iniciar sesion: ")
