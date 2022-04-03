beaecb5f779d9653e36d7d992883be9dea05f01b
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-03 13:24:18 +0200

    no puedo definir una variable de re

--- Clases/correo.py
+++ Clases/correo.py
@@ -1,18 +1,21 @@
 import re #recibe una cadena y regresa un booleano indicando si es un correo electrónico válido o no
 class Correoelectronico:
-    def __init__(self,correo):
+    def __init__(self,correo,valido):
         self.correo = correo
-    def search1():
-        return re.search("@",correo)
-    def validacion():
-        search1()
-        if re.search == True:
+        self.valido = valido
+    def search1(self):
+        valido = re.search("@",self.correo)
+        if self.valido == True:
             print("hola")
-        elif re.search == False:
-            print("Adios")
+        elif self.valido == False:
+            print("Adios")    
+
 
 
 
 correo = input("Introduzca su correo para poder iniciar sesion: ")
-ff= Correoelectronico(correo)
-print(ff.validacion())
+re.search("@",correo)
+if re.search("@",correo):
+    print("hola")
+elif re.search("@",correo) == None:
+    print("Adios")
